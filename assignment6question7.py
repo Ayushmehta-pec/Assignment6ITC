49d85c8fc05b47511a6d6e784ee2a060ecb32928
Ayushmehta-pec <121235469+Ayushmehta-pec@users.noreply.github.com>
2023-01-27 21:16:55 +0530

    Update assignment6question7.py

--- assignment6question7.py
+++ assignment6question7.py
@@ -1,20 +1,14 @@
-class sol:
- def findTriplets(arr, n):
+class Student:
+    pass
+class Marks:
+    pass
+student_1 = Student()
+marks_1 = Marks()
 
-    found = False
-    for i in range(0, n-2):
-
-        for j in range(i+1, n-1):
-
-            for k in range(j+1, n):
-
-                if (arr[i] + arr[j] + arr[k] == 0):
-                    print(arr[i], arr[j], arr[k])
-                    found = True
-    if (found == False):
-        print(" not exist ")
-
-
-arr = [-25, -10, -7, -3, 2, 4, 8, 10]
-n = len(arr)
-sol.findTriplets(arr, n)
+print(isinstance(student_1, Student))
+print(isinstance(marks_1, Student))
+print(isinstance(marks_1, Marks)) 
+print(isinstance(student_1, Marks))
+print("\nCheck whether the said classes are subclasses of the built-in object class or not.")
+print(issubclass(Student, object))
+print(issubclass(Marks, object))
