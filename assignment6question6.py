9a8c84d9d8e8b77f47eb6291b49d73f9a4cb2593
Ayushmehta-pec <121235469+Ayushmehta-pec@users.noreply.github.com>
2023-01-27 21:15:57 +0530

    Update assignment6question6.py

--- assignment6question6.py
+++ assignment6question6.py
@@ -1,14 +1,12 @@
-class Student:
-    pass
-class Marks:
-    pass
-student_1 = Student()
-marks_1 = Marks()
+def student_data(student_id, **kwargs):
+    print(f'\nStudent ID: {student_id}')
+    if 'student_name' in kwargs:
+        print(f"Student Name: {kwargs['student_name']}")
 
-print(isinstance(student_1, Student))
-print(isinstance(marks_1, Student))
-print(isinstance(marks_1, Marks)) 
-print(isinstance(student_1, Marks))
-print("\nCheck whether the said classes are subclasses of the built-in object class or not.")
-print(issubclass(Student, object))
-print(issubclass(Marks, object))
+    if 'student_class' in kwargs:
+        print(f"Student Class: {kwargs['student_class']}")
+
+
+student_data(student_id='22104099', student_name='Ayush Mehta')
+
+student_data(student_id='22104099', student_name='Ayush Mehta', student_class='G2')
